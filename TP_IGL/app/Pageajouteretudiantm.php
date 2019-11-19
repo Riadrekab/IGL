162818307b6d83843bf18bfea45a982a54003395
Riad Rekab <hr_rekab@esi.dz>
2019-11-19 20:55:58 +0100

    Changement au niveau de la classe managerEtudiant pour corriger une erreur relié à la bdd

--- TP_IGL/app/Pageajouteretudiantm.php
+++ TP_IGL/app/Pageajouteretudiantm.php
@@ -5,10 +5,10 @@ require __DIR__.'/Classes/managerEtudiant.php';
 function AjoutEtuP()
 {
     $bdd = new PDO('mysql:host=localhost;dbname=scolaritébdd;charset=utf8', 'root', '',
-        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));    
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     //$etudiants_req= $bdd->query('SELECT * FROM etudiants');
     $etudiant=new etudiant();
-    $donne= array("Matricule"=>"121321", "Nom"=>"Rekab", "Prenom"=>"Riad");
+    $donne= array("Matricule"=>'123132', "Nom"=>"WAW", "Prenom"=>"Riad");
     $etudiant->hydrate($donne);
     $manager = new managerEtudiant($bdd);
     $manager->addEtu($etudiant);
