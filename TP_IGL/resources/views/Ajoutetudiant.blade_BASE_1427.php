e84b6caa176bf06e6db72657c2aa43172ecdf258
Riad Rekab <hr_rekab@esi.dz>
2019-11-22 19:11:59 +0100

    Ajout d'une fonction de hashage pour mot de passe

---
+++ TP_IGL/resources/views/Ajoutetudiant.blade_BASE_1427.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+
+</head>
+<body>
+<h1>Ajouter etudiant</h1>
+<form method="POST" action="http://localhost/IGL/TP_IGL/public/index.php/ajouter_etudiant" accept-charset="UTF-8">
+    {{csrf_field()}}
+    <label for="matricule">Matricule: </label>
+    <input name="matricule" type="text" id="matricule">
+    <label for="nom">Nom: </label>
+    <input name="nom" type="text" id="nom">
+    <label for="prenom">Prenom: </label>
+    <input name="prenom" type="text" id="prenom">
+
+    <input type="submit" value="Envoyer !">
+</form>
+</body>
+</html>
---
+++ TP_IGL/resources/views/Ajoutetudiant.blade_REMOTE_1427.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <title>Ajouter</title>
+    
+</head>
+<body>
+
+<div id="app" class="container">
+        <form method="POST" action="http://localhost/IGL/TP_IGL/public/index.php/ajouter_etudiant" accept-charset="UTF-8">
+        <legend><span class="titre"></span>Ajouter Etudiant</legend>
+        <hr>
+        <fieldset>
+                <label for="matricule">Matricule: </label>
+                <input name="matricule" type="text" id="matricule" placeholder="Matricule*">
+                <label for="nom">Nom: </label>
+                <input name="nom" type="text" id="nom" placeholder="Nom*">
+                <label for="prenom">Prenom: </label>
+                <input name="prenom" type="text" id="prenom" placeholder="Prenom*"> 
+              
+                
+        </fieldset>
+        <input type="submit"/>
+        </form>
+        </div></body>
+<script src="{{ asset('js/app.js') }}"></script>
+</html>
