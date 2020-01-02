883f352aed458711a76fbbec62699ea207bed505
Riad Rekab <hr_rekab@esi.dz>
2020-01-02 09:23:24 +0200

    Test unitaire du microservice ConsulterAbsenceEtudiant finalisé

--- Back-end/Consulter_absence_microservice/tests/Unit/ConsulterEtudiantTest.php
+++ Back-end/Consulter_absence_microservice/tests/Unit/ConsulterEtudiantTest.php
@@ -5,20 +5,42 @@ use App\Http\Controllers\AbsencesController;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use PDO;
+use App\Classes\absence;
 
 
 class ConsulterEtudiantTest extends TestCase
 {
     public  function testPrintabs()
     {
+
+        $bdd = new PDO('mysql:host=localhost;dbname=scolaritebdd;charset=utf8', 'root', '');
+        $test12= $bdd->query('SELECT * from absences WHERE Matricule =\'10/1222\'' );
         $absC=new AbsencesController();
         $mat="10/1222";
-        $abs=[];
+
         $abs=$absC->index($mat);
-        foreach ($abs as $value) {
-            print_r($value);
+        $ok=$test12->fetchAll( PDO::FETCH_CLASS, "App\Classes\absence" );
+
+        $booleg=false;
+       $i=0;
+       $tableau=[];
+       foreach ($ok as $value)
+       {
+           $tableau[$i]=$value->Matiere;
+           $i++;
+
+       }
+        $i2=0;
+        foreach ($abs as $resulttest)
+        {
+           if ($resulttest["Matiere"]==$tableau[$i2])
+           {
+               $booleg=true;
+           }
+           $i2++;
         }
-        $this->assertTrue(true,"true");
+        $this->assertTrue($booleg,"true");
 
 
 
